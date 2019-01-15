9b0693f1a932d579d33f4f85e4bf15cc20daecac
Zhangsu Wen <zswen@zhangsudembp.lan>
2019-01-14 23:52:05 -0800

    Fixed ImportError. Now we can run tests.py in any directory

--- test/tests.py
+++ test/tests.py
@@ -1,8 +1,11 @@
-
-import sys
+import os,sys
+relpath = os.path.dirname(__file__) # Find relative path: from current directory to test.py
+abspath = os.path.abspath(".")      # Find absolute path: from / to current directory
+os.chdir(abspath + '/' + relpath)
 sys.path.append('../')
 from first_timers import first_timers
 import json
+
 
 example_res = json.load(open('../data/example.json', 'r'))
 example_issues = example_res['items']
