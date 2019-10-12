7c35eca881c8f070bc1497df60840bf9aa9c72b7
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-10-12 23:30:18 +0530

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ echo "Docker container has been started"
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
+python3 first_timers/run.py --creds-path credentials.json --db-path data/db.json --only-save --create
+python3 first_timers/run.py --creds-path credentials.json --db-path data/db.json --only-save
+
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
