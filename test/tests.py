9a75544734bb311296459fce494f26deed6a9948
Ashok B <lifebalance@gmail.com>
2022-01-14 17:20:07 +0530

    refactored imports and added docstrings

--- test/tests.py
+++ test/tests.py
@@ -1,17 +1,20 @@
-import os,sys
+import os
+import sys
+import first_timers
+import json
+
+
 relpath = os.path.dirname(__file__) # Find relative path: from current directory to test.py
 abspath = os.path.abspath(".")      # Find absolute path: from / to current directory
 os.chdir(abspath + '/first_timers')
 sys.path.append('./')
-import first_timers as first_timers
-import json
-
 
 example_res = json.load(open('../data/example.json', 'r'))
 example_issues = example_res['items']
 
 
 def test_fetcher():
+    """Test whether first_timer_issues are getting picked up."""
     issue_list = first_timers.get_first_timer_issues()
     assert len(issue_list) > 0
 
@@ -20,6 +23,7 @@ test_fetcher.__setattr__('__test__', False)  # Test disabled by default.
 
 
 def test_get_fresh():
+    """Test whether fresh issues are retrieved."""
     new_issues = first_timers.get_fresh(example_issues[:-1], example_issues)
     assert new_issues[0] == example_issues[-1]
 
