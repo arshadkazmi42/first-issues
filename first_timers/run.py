1817baf5de3082cf64c9a4b25cd3f5eadafeed7e
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-07 08:49:32 +0300

    Add issues labels to fetch

--- first_timers/run.py
+++ first_timers/run.py
@@ -47,7 +47,11 @@ def run(only_save, db_path, create, creds_path, debug):
         sys.exit(-1)
 
     # Getting the latest list of issues from GitHub
-    new_issues = FT.get_first_timer_issues()
+    issue_labels = ('good first issue', 'good-first-issue')
+    new_issues = []
+    for label in issue_labels:
+        new_issues.append(FT.get_first_timer_issues(label))    
+
     fresh_issues = FT.get_fresh(old_issues, new_issues)
     all_issues = fresh_issues + old_issues
 
