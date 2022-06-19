66d9e5e01d0b5e06bd631f1bd3cefdd61d0c4592
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2022-06-19 11:57:49 +0530

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,11 @@
+# Included this file at the root level for future testing scenarios 
+# as per https://stackoverflow.com/a/34520971/307454
+# sample fixture code provided below
+"""
+import pytest
+
+@pytest.fixture
+def input_value():
+   input = 39
+   return input
+"""
