29afece1b3a59c875ba843e95e84da109a542090
AniketTripathi05 <aniket.tripathi2005@gmail.com>
2024-01-06 18:02:29 +0530

    Update conftest.py
    done a bit of change

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 # Included this file at the root level for future testing scenarios 
 # as per https://stackoverflow.com/a/34520971/307454
-# sample fixture code provided below
+# sample of fixture code provided below
 """
 import pytest
 
